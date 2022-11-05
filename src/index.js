0d7784e2a82955361af4a0f2bd50dce1570bbe5b
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2022-11-05 22:26:05 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/job_page/">
     <React.StrictMode>
       <Suspense fallback={<h1>LOADDDDDDDDDEEERR</h1>}>
         <App />
